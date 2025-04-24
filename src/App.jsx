1bbbf5fefd54fbb3b95cfe8588df013db4944b61
Anirudh Dubey <anirudhdubey@Anirudhs-MacBook-Pro.local>
2025-04-24 15:39:23 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Navbar/> 
-    </>
-  )
+    <div className="px-5 bg-zinc-950 min-h-screen">
+      <Navbar />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Linkedin, Github, Moon } from "lucide-react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center text-white py-2 text-2xl">
+      <div className="flex items-center font-semibold">
+        <span className="text-red-400 mr-2">
+          (<span className="text-sky-500">&lt;/&gt;</span>)
+        </span>
+        Web Developer
+      </div>
+
+      <div className="flex items-center gap-4">
+        <a
+          href="https://www.linkedin.com/in/anirudh--dubey/"
+          target="_blank"
+          className="bg-gray-700 hover:bg-gray-500 p-2 rounded-full"
+        >
+          <Linkedin size={25} />
+        </a>
+        <a
+          href="https://github.com/Anirudh2244"
+          target="_blank"
+          className="bg-gray-700 hover:bg-gray-500 p-2 rounded-full"
+        >
+          <Github size={25} />
+        </a>
+        <a href="#" className="bg-gray-700 hover:bg-gray-500 p-2 rounded-full">
+          <Moon size={25} />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
