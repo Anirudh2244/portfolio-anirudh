29418cefa344fed868fa8dbc478555ac3280f2ed
Anirudh Dubey <anirudhdubey@Anirudhs-MacBook-Pro.local>
2025-04-24 17:23:24 +0530

    Hero section text

---
+++ src/components/ExBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExBar() {
+  return <div className="w-full sm:max-w-6/12 bg-slate-950">ExBar</div>;
+}
+
+export default ExBar;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import ExBar from "./ExBar";
 
 function HeroSection() {
   return (
-    <div className="text-white">
-      <div className="font-bold text-4xl">Hello! I'm Anirudh Dubey</div>
-      <p></p>
+    <div className="text-white flex flex-col justify-center items-center gap-4 bg-gray-900 sm:p-15 p-5">
+      <div className="font-bold sm:text-5xl text-3xl text-center">
+        Hello! I'm Anirudh Dubey
+      </div>
+      <p>
+        I am passionate about delivering pixel-perfect UI with smooth
+        functionality while maintaining code readability and performance. My
+        goal is to create engaging and accessible web applications that leave a
+        lasting impact. Feel free to explore my projects, and let’s connect if
+        you’re looking to collaborate or build something amazing together!
+      </p>
+      <ExBar />
     </div>
   );
 }
